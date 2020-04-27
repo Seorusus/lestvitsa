415e9d63b917bd61b7af327ab9393de573bf0cae
Oleg Kreminsky <oleg.kreminsky@ffwagency.com>
2020-04-27 12:39:05 +0300

    Composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,26 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+      0 => '9999999-dev',
+    ),
+    'reference' => 'd32c2ded71355b851cae459d0834574c58ae7a9b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'd32c2ded71355b851cae459d0834574c58ae7a9b',
+    ),
+  ),
+);
